da3f598911e33b863f5660f676afcb835b05be8c
mrbojan <bartlomiej.piotr.bojanowski@gmail.com>
2019-03-20 10:02:27 +0100

    Login view, configuration database and check errors

---
+++ login_view.php
@@ -0,0 +1,42 @@
+<?php include('php/login.php') ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Galeria zdjęć</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" 
+	integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" 
+	integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossorigin="anonymous">
+	<link rel="stylesheet"  href="css/login.css" type="text/css">
+	
+</head>
+<body>
+	<div class="bg">
+		<div class="login">
+
+			<h2>Zaloguj się na konto</h2>
+
+			<form method="post" action="login_view.php">
+
+				<?php include('php/errors.php'); ?>
+
+
+					<input type="text" name="username" placeholder="nazwa użytkownika" >
+
+
+					<input type="password" name="password" placeholder="hasło">
+
+
+					<button type="submit" class="btn" name="login_user">Zaloguj się</button>
+
+
+					<a class="signup" href="register.php">Zarejestruj się</a>
+
+			</form>
+	</div>
+	</div>
+</body>
+</html>
---
+++ php/login.php
@@ -0,0 +1,39 @@
+<?php
+	session_start();
+	include('dbConfig.php');	
+	// variable declaration
+	
+	$username = "";
+	$email    = "";
+	$errors = array(); 
+	$folder_name = "";
+	$_SESSION['success'] = "";
+	
+	//check username and password
+	if (isset($_POST['login_user'])) :	
+		$username = mysqli_real_escape_string($db, $_POST['username']);
+		$password = mysqli_real_escape_string($db, $_POST['password']);
+
+		if (empty($username)) :
+			array_push($errors, "Username is required");
+		endif;
+		
+		if (empty($password)) :
+			array_push($errors, "Password is required");
+		endif;
+
+		if (count($errors) == 0) :
+			$password = md5($password);
+			$query = "SELECT * FROM users WHERE username='$username' AND password='$password'";
+			$results = mysqli_query($db, $query);
+
+			if (mysqli_num_rows($results) == 1) :
+				$_SESSION['username'] = $username;
+				$_SESSION['success'] = "You are now logged in";
+				header('location: login_view.php');
+			else :
+				array_push($errors, "Wrong username/password combination");
+			endif;
+		endif;
+	endif;
+?>
