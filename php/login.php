a9c762e6812e15c210c4608bc1738b81aaa7f603
mrbojan <bartlomiej.piotr.bojanowski@gmail.com>
2019-04-03 10:10:31 +0200

    Show photos without points and place

--- php/login.php
+++ php/login.php
@@ -30,7 +30,7 @@
 			if (mysqli_num_rows($results) == 1) :
 				$_SESSION['username'] = $username;
 				$_SESSION['success'] = "You are now logged in";
-				header('location: ../gallery_view.php');
+				header('location: gallery_view.php');
 			else :
 				array_push($errors, "Wrong username/password combination");
 			endif;
